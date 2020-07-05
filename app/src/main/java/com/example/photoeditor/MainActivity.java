3f7172ab39da1b69a734c782fb152673a22b5844
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-05 14:11:04 +0200

    In MainActivity implements FiltersListFragmentListener, EditImageFragmentListener, init views, added setupViewPager()

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -1,16 +1,106 @@
 package com.example.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.photoeditor.Adapter.ViewPagerAdapter;
+import com.example.photoeditor.Interface.EditImageFragmentListener;
+import com.example.photoeditor.Interface.FiltersListFragmentListener;
+import com.google.android.material.tabs.TabLayout;
+import com.zomato.photofilters.imageprocessors.Filter;
+
+public class MainActivity extends AppCompatActivity implements FiltersListFragmentListener, EditImageFragmentListener {
 
     public static final String pictureName = "flash.jpg";
+    public static final int PERMISSION_PICK_IMAGE = 1000;
+
+    ImageView img_preview;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    CoordinatorLayout coordinatorLayout;
+
+    Bitmap originalBitmap, filteredBitmap, finalBitmap;
+
+    FilterListFragment filtersListFragment;
+    EditImageFragment editImageFragment;
+
+    int brightnessFinal = 0;
+    float saturationFinal = 1.0f;
+    float contrastFinal = 1.0f;
+
+    //Load native image filters lib
+    static {
+        System.loadLibrary("NativeImageProcessor");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Instagram Filter");
+
+        //view
+        img_preview = (ImageView) findViewById(R.id.image_preview);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        filtersListFragment = new FilterListFragment();
+        filtersListFragment.setListener(this);
+
+        editImageFragment = new EditImageFragment();
+        editImageFragment.setListener(this);
+
+        adapter.addFragment(filtersListFragment, "FILTERS");
+        adapter.addFragment(editImageFragment, "EDIT");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBrightnessChanged(int brightness) {
+
+    }
+
+    @Override
+    public void onSaturationChanged(float saturation) {
+
+    }
+
+    @Override
+    public void onContrastChanged(float contrast) {
+
+    }
+
+    @Override
+    public void onEditStarted() {
+
+    }
+
+    @Override
+    public void onEditCompleted() {
+
+    }
+
+    @Override
+    public void onFilterSelected(Filter filter) {
+
     }
 }
