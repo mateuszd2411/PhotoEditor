bdad3440cb069ca67f283bfcfe7ef796169c32df
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-27 11:15:33 +0200

    In EmojiFragment set recycler_emoji

--- app/src/main/java/com/example/photoeditor/EmojiFragment.java
+++ app/src/main/java/com/example/photoeditor/EmojiFragment.java
@@ -3,12 +3,35 @@ package com.example.photoeditor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EmojiFragment extends Fragment {
+import com.example.photoeditor.Adapter.EmojiAdapter;
+import com.example.photoeditor.Interface.EmojiFragmentListener;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+
+public class EmojiFragment extends BottomSheetDialogFragment implements EmojiAdapter.EmojiAdapterListener {
+
+    RecyclerView recycler_emoji;
+    static EmojiFragment instance;
+
+    EmojiFragmentListener listener;
+
+    public void setListener(EmojiFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    public static EmojiFragment getInstance() {
+        if (instance == null)
+            instance = new EmojiFragment();
+        return instance;
+    }
 
     public EmojiFragment() {
         // Required empty public constructor
@@ -18,6 +41,20 @@ public class EmojiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emoji, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_emoji, container, false);
+
+        recycler_emoji = (RecyclerView) itemView.findViewById(R.id.recycler_emoji);
+        recycler_emoji.setHasFixedSize(true);
+        recycler_emoji.setLayoutManager(new GridLayoutManager(getActivity(), 5));
+
+        EmojiAdapter adapter = new EmojiAdapter(getContext(), PhotoEditor.getEmojis(getContext()), this);
+        recycler_emoji.setAdapter(adapter);
+
+        return itemView;
+    }
+
+    @Override
+    public void OnEmojiItemSelected(String emoji) {
+        listener.onEmojiSelected(emoji);
     }
 }
---
+++ app/src/main/java/com/example/photoeditor/Interface/EmojiFragmentListener.java
@@ -0,0 +1,5 @@
+package com.example.photoeditor.Interface;
+
+public interface EmojiFragmentListener {
+    void onEmojiSelected(String emoji);
+}
