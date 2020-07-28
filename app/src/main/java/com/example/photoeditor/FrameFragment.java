e01004d7c4faad04abbef4f163b0f7ef710714ea
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-28 17:58:20 +0200

    FrameFragment set recycler frame view

--- app/src/main/java/com/example/photoeditor/FrameFragment.java
+++ app/src/main/java/com/example/photoeditor/FrameFragment.java
@@ -2,7 +2,7 @@ package com.example.photoeditor;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -10,10 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class FrameFragment extends Fragment {
+import com.example.photoeditor.Adapter.FrameAdapter;
+import com.example.photoeditor.Interface.AddFrameListener;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class FrameFragment extends BottomSheetDialogFragment implements FrameAdapter.FrameAdapterListener {
 
     RecyclerView recycler_frame;
     Button brn_add_frame;
+
+    int frame_selected = -1;
+
+    AddFrameListener listener;
+
+    public void setListener(AddFrameListener listener) {
+        this.listener = listener;
+    }
 
     static FrameFragment instance;
 
@@ -32,6 +44,27 @@ public class FrameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frame, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_frame, container, false);
+
+        recycler_frame = (RecyclerView) itemView.findViewById(R.id.recycler_frame);
+        brn_add_frame = (Button) itemView.findViewById(R.id.btn_add_frame);
+
+        recycler_frame.setHasFixedSize(true);
+        recycler_frame.setLayoutManager(new LinearLayoutManager(getContext()));
+        recycler_frame.setAdapter(new FrameAdapter(getContext(), this));
+
+        brn_add_frame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onAddFrame(frame_selected);
+            }
+        });
+
+        return itemView;
+    }
+
+    @Override
+    public void omFrameSelected(int frame) {
+        frame_selected = frame;
     }
 }
